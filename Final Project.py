71bfec10458971abb24d5607fc232bd31d499040
kakie19 <31842912+kakie19@users.noreply.github.com>
2023-05-12 11:23:27 +0800

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QDesktopWidget, QLabel, QFileDialog
+
 
 class App(QWidget):
 
@@ -8,7 +9,7 @@ class App(QWidget):
         super().__init__()
         self.title = 'Encryption and Hashing'
         self.width = 500
-        self.height = 300
+        self.height = 170
         self.initUI()
 
     def initUI(self):
@@ -33,6 +34,14 @@ class App(QWidget):
         btn_hashing = QPushButton('Hashing(SHA-256)', self)
         btn_hashing.clicked.connect(lambda: subprocess.call(['python', 'Hashing.py']))
         layout.addWidget(btn_hashing)
+        
+        btn_verify = QPushButton('Verify SHA-256', self)
+        btn_verify.clicked.connect(self.verify)
+        layout.addWidget(btn_verify)
+
+        # Add a label to display the hash verification result
+        self.lbl_result = QLabel()
+        layout.addWidget(self.lbl_result)
 
         # Add the layout to the window
         self.setLayout(layout)
@@ -48,6 +57,26 @@ class App(QWidget):
         # Set the position of the window
         self.move(center_x, center_y)
 
+    def verify(self):
+        # Get the file paths to the hashed values
+        file_path1, _ = QFileDialog.getOpenFileName(self, 'Open Hash File 1', '', 'Hash files (*.txt)')
+        file_path2, _ = QFileDialog.getOpenFileName(self, 'Open Hash File 2', '', 'Hash files (*.txt)')
+
+        if not file_path1 or not file_path2:
+            # Return if any file path is empty
+            return
+
+        # Read the hashed values from the files
+        with open(file_path1, 'rb') as file:
+            hash1 = file.read()
+        with open(file_path2, 'rb') as file:
+            hash2 = file.read()
+
+        # Compare the hashed values
+        if hash1 == hash2:
+            self.lbl_result.setText('Hashes match!')
+        else:
+            self.lbl_result.setText('Hashes do not match.')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
